324191b99472e6cbc64777554cf93e0897c97852
Praveen0825 <91229praveenkumar@gmail.com>
2025-06-14 23:17:44 +0530

    Main.java updated in TicTacToe design

--- TIcTacToe/src/Main.java
+++ TIcTacToe/src/Main.java
@@ -1,5 +1,13 @@
+import entity.Game;
+import entity.Player;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Player player1 = new Player("Player 1", 'X');
+        Player player2 = new Player("Player 2", 'O');
+
+        Game game = new Game(3,player1, player2);
+        game.play();
+        //System.out.println("Hello world!");
     }
 }
